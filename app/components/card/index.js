b6ecb6f536cecac16524f0479bcfa0be6f4e9cd8
William Owen <william.owen.bsc@gmail.com>
2024-12-16 16:40:21 -0800

    Add newline at the end of card component definition; improve code formatting

--- app/components/card/index.js
+++ app/components/card/index.js
@@ -14,6 +14,7 @@ createCustomElement('card-component', function() {
     //@TODO Test the card component by creating new card elements
 
     
+
 }, cardTemplate, style, {
     attributes: ['rank', 'suit']
 })
